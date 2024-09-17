8f2a670de17f95b72dba16111b3903fbb0a75159
Victor C <victorcordeiro.contact@gmail.com>
2024-09-17 14:07:04 -0300

    add: category

---
+++ src/main/java/com/example/demo/entities/Category.java
@@ -0,0 +1,61 @@
+package com.example.demo.entities;
+import java.io.Serializable;
+import java.util.Objects;
+
+// Serializable -> utilizado para que o objeto java possa ser convertido em uma sequencia de Bytes
+public class Category implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String name;
+
+    public Category(){}
+    
+    public Category(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+    
+
+}
