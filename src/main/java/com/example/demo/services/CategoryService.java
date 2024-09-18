ed4a84089f4e419f345ebec03b8a293a3920cb2c
Victor C <victorcordeiro.contact@gmail.com>
2024-09-18 10:59:48 -0300

    add: Transactional notation / orElseThrow method to catch message error

--- src/main/java/com/example/demo/services/CategoryService.java
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -1,14 +1,18 @@
 package com.example.demo.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dto.CategoryDTO;
 import com.example.demo.entities.Category;
 import com.example.demo.repository.CategoryRepository;
+import com.example.demo.services.exceptions.EntityNotFoundException;
+
 
 // Registra um componente como sendo injetável pelo Spring -> Habilita a DI
 @Service
@@ -17,9 +21,23 @@ public class CategoryService {
     @Autowired // Injetando uma instância de um componente que é gerenciada pelo Spring
     private CategoryRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category> list = repository.findAll();
         return list.stream().map( x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        //Optional serve para trabalhar com valores que podem ou não serem nulos
+        Optional<Category> obj = repository.findById(id);
+
+        // Com o método get(), o Optional retorna a entidade
+        //Category entity = obj.get();
+        
+        // O método orElseThrow conseguimos retornar outro valor se for nulo
+        // Utilizei um Exception personalizado que criei como subpacote da camada services
+        Category entity = obj.orElseThrow( () -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
 }
