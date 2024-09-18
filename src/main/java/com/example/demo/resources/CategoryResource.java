587308f5877257f28cde221e32a60c64ac32338d
Victor C <victorcordeiro.contact@gmail.com>
2024-09-18 11:37:20 -0300

    add: insertCategory router

--- src/main/java/com/example/demo/resources/CategoryResource.java
+++ src/main/java/com/example/demo/resources/CategoryResource.java
@@ -1,16 +1,21 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.dto.CategoryDTO;
 import com.example.demo.services.CategoryService;
+
 
 // Annotation -> configura uma classe em um recurso Rest 
 @RestController
@@ -29,10 +34,17 @@ public class CategoryResource { //Se o recurso tem relação com entidade ->enti
        return ResponseEntity.ok().body(list);
     }
 
+    // path variables 
     @GetMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
        CategoryDTO dto = service.findById(id);
        return ResponseEntity.ok().body(dto);
     }
-
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insertCategory(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 }
+ 
