62a16ffca9f7173f670084ae2be84b771db55be1
Victor C <victorcordeiro.contact@gmail.com>
2024-10-15 16:05:52 -0300

    add listening exception on layer of controller

--- src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -7,22 +7,49 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.demo.services.exceptions.DatabaseException;
 import com.example.demo.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
-
-//Intercepta exceptions pela camada
 @ControllerAdvice
 public class ResourceExceptionHandler {
+    /**
+     * Handles ResourceNotFoundException and returns a ResponseEntity with StandardError.
+     * 
+     * @param e The ResourceNotFoundException that was thrown.
+     * @param request The HttpServletRequest associated with the exception.
+     * @return A ResponseEntity containing a StandardError object with details about the exception.
+     */
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
-        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setStatus(status.value());
         err.setError("Resource not found");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+        return ResponseEntity.status(status).body(err);
+    }
+
+    /**
+     * Handles DatabaseException and returns a ResponseEntity with StandardError.
+     * 
+     * @param e The DatabaseException that was thrown.
+     * @param request The HttpServletRequest associated with the exception.
+     * @return A ResponseEntity containing a StandardError object with details about the exception.
+     */
+    
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value()); 
+        err.setError("Database exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
 
     }
 }
