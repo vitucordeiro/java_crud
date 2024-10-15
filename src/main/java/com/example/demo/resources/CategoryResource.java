987b5ce53c1ef0927d446f2687aee9c3a40b4851
Victor C <victorcordeiro.contact@gmail.com>
2024-10-15 16:04:16 -0300

    add router to delete category

--- src/main/java/com/example/demo/resources/CategoryResource.java
+++ src/main/java/com/example/demo/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,10 +49,17 @@ public class CategoryResource { //Se o recurso tem relação com entidade ->enti
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+    
     @PutMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> insert(@PathVariable Long id, @RequestBody CategoryDTO dto ){
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
+    }
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build(); //Return 204 e o corpo retorna vázio
     }
 }
  
