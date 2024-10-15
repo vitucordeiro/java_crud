422b04cca153e42c2805291e9bfee29d42fab5ee
Victor C <victorcordeiro.contact@gmail.com>
2024-10-15 16:36:02 -0300

    refactor: changing the params of the findAll endpoint

--- src/main/java/com/example/demo/resources/CategoryResource.java
+++ src/main/java/com/example/demo/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.example.demo.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -31,10 +34,20 @@ public class CategoryResource { //Se o recurso tem relação com entidade ->enti
 
     // Encapsula uma resposta HTTP -> Generic
     @GetMapping()
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-       List<CategoryDTO> list = service.findAll();
-       return ResponseEntity.ok().body(list);
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+        @RequestParam(value="page", defaultValue="0") Integer page,
+        @RequestParam(value="linesPerPage", defaultValue="12") Integer linesPerPage,
+        @RequestParam(value="direction", defaultValue="ASC") String direction,
+        @RequestParam(value="orderBy", defaultValue="name") String orderBy
+    ){  //Retornará uma Page -> Paginação
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> rawPage = service.findAllPaged(pageRequest);
+        return ResponseEntity.ok().body(rawPage);
     }
+
+    //Notas: Quando é obrigatório ser passado um argumento, usa-se @PathVariable. Quando não é, utiliza-se @RequestParams(value="", defaultValue= x ) Type var 
+    //Exemplo abaixo, poderia passar o @RequestParams(value="id", defaultValue=1) Integer id 
 
     // path variables 
     @GetMapping(value = "/{id}")
