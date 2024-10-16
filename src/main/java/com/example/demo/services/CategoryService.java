463c2facabdc185fb3fe5899a7938dfd55e4cf81
Victor C <victorcordeiro.contact@gmail.com>
2024-10-16 15:12:32 -0300

    add a catch to EmpytResultDataAccessException on Category Service

--- src/main/java/com/example/demo/services/CategoryService.java
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -72,7 +73,9 @@ public class CategoryService {
             }
             repository.deleteById(id);
         } 
-        
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
         catch (DataIntegrityViolationException e) { 
             throw new DatabaseException("Integrity violation");
         }
